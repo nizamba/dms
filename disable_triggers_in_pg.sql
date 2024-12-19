008c77812e865b17b5705fa3fa8bea73b636fdff
nizamb <nizam.bader@niceactimize.com>
2024-12-19 15:08:09 +0200

    disable triggers script

--- disable_triggers_in_pg.sql
+++ disable_triggers_in_pg.sql
@@ -1,16 +1,20 @@
--- Create a table to store the trigger enable/disable commands
-do $$
-begin
-CREATE TABLE target_schema_name.trigger_commands (
-    disable_command text,
-    enable_command text
-);
--- Insert values from the trigger query into the trigger_commands table
-INSERT INTO target_schema_name.trigger_commands (disable_command, enable_command)
-SELECT 
-    'alter table '||event_object_schema||'.'||event_object_table||' disable trigger '||trigger_name||';' as disable_command,
-    'alter table '||event_object_schema||'.'||event_object_table||' enable trigger '||trigger_name||';' as enable_command
-FROM information_schema.triggers  
-WHERE event_object_schema='target_schema_name';
-SELECT * FROM target_schema_name.trigger_commands;
-end; $$
+DO $$
+BEGIN
+    -- Create the table if it does not exist
+    IF NOT EXISTS (SELECT 1 FROM information_schema.tables
+                   WHERE table_schema = 'target_schema_name'
+                   AND table_name = 'trigger_commands') THEN
+        CREATE TABLE target_schema_name.trigger_commands (
+            disable_command TEXT,
+            enable_command TEXT
+        );
+    END IF;
+
+    -- Insert trigger commands into the table
+    INSERT INTO target_schema_name.trigger_commands (disable_command, enable_command)
+    SELECT
+        'ALTER TABLE ' || event_object_schema || '.' || event_object_table || ' DISABLE TRIGGER ' || trigger_name || ';' AS disable_command,
+        'ALTER TABLE ' || event_object_schema || '.' || event_object_table || ' ENABLE TRIGGER ' || trigger_name || ';' AS enable_command
+    FROM information_schema.triggers
+    WHERE event_object_schema = 'target_schema_name';
+END $$;
